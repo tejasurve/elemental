1a734215368f6fcc853b74506e96e90f4c5ce9e4
tejasurve <tejas.surve358@gmail.com>
2022-05-04 16:36:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
     <Router>
       <Navbar  />
       <Routes>
-        <Route path='/' element={<MainBody />} />
+        <Route path='/elemental' element={<MainBody />} />
         <Route path='/shop' />
         <Route path='/aboutus' />
         <Route path='/myworkbooks'  />
